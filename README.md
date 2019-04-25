68571a9d41c5c390f0a6b091acd6b779ecdbdc8e
Frantisek Rokusek <frantisek.rokusek@gmail.com>
2019-04-25 08:58:33 +0200

    modified python script

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Liquid flow_controler
 Try to figure out how to control a flow of liquid (water, beer, wine,...) and to count the liquides passed through.
 
-# Sources of inspiration :
-- Motor 12VDC + mosfet + resistanc
-  http://wiki.mchobby.be/index.php?title=Mini_Kit_Moteur_Hobbyiste
-- Adafruit tutorial and code from Github
-  https://github.com/adafruit/Adafruit-Flow-Meter
+## Sources of inspiration :
+- 'Motor 12VDC + mosfet + resistanc'(http://wiki.mchobby.be/index.php?title=Mini_Kit_Moteur_Hobbyiste)
+- 'Adafruit tutorial and code from Github'(https://github.com/adafruit/Adafruit-Flow-Meter)
 
 Connect the red wire to +5V, the black wire to common ground and the yellow sensor wire to pin #2.
 
+
+## Products / devices
+1. Raspberry PI3B+
+2. Flowmetter()
--- control.py
+++ control.py
@@ -1,44 +1,45 @@
+# if needed to link a screen to the device
 #include "LiquidCrystal.h"
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+#LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
-// which pin to use for reading the sensor? can use any pin!
-#define FLOWSENSORPIN 2
+#// which pin to use for reading the sensor? can use any pin!
+define FLOWSENSORPIN 2
 
-// count how many pulses!
+#// count how many pulses!
 volatile uint16_t pulses = 0;
-// track the state of the pulse pin
+#// track the state of the pulse pin
 volatile uint8_t lastflowpinstate;
-// you can try to keep time of how long it is between pulses
+#// you can try to keep time of how long it is between pulses
 volatile uint32_t lastflowratetimer = 0;
-// and use that to calculate a flow rate
+#// and use that to calculate a flow rate
 volatile float flowrate;
-// Interrupt is called once a millisecond, looks for any pulses from the sensor!
+#// Interrupt is called once a millisecond, looks for any pulses from the sensor!
 SIGNAL(TIMER0_COMPA_vect) {
   uint8_t x = digitalRead(FLOWSENSORPIN);
 
   if (x == lastflowpinstate) {
     lastflowratetimer++;
-    return; // nothing changed!
+    return; #// nothing changed!
   }
 
   if (x == HIGH) {
-    //low to high transition!
+    #//low to high transition!
     pulses++;
   }
   lastflowpinstate = x;
   flowrate = 1000.0;
-  flowrate /= lastflowratetimer;  // in hertz
+  flowrate /= lastflowratetimer;  #// in hertz
   lastflowratetimer = 0;
 }
 
 void useInterrupt(boolean v) {
   if (v) {
-    // Timer0 is already used for millis() - we'll just interrupt somewhere
-    // in the middle and call the "Compare A" function above
+    #// Timer0 is already used for millis() - we'll just interrupt somewhere
+    #// in the middle and call the "Compare A" function above
     OCR0A = 0xAF;
     TIMSK0 |= _BV(OCIE0A);
   } else {
-    // do not call the interrupt function COMPA anymore
+    #// do not call the interrupt function COMPA anymore
     TIMSK0 &= ~_BV(OCIE0A);
   }
 }
@@ -54,17 +55,18 @@ void setup() {
    useInterrupt(true);
 }
 
-void loop()                     // run over and over again
+void loop()                     #// run over and over again
 {
+  #this is the LCD part of the program
   lcd.setCursor(0, 0);
   lcd.print("Pulses:"); lcd.print(pulses, DEC);
   lcd.print(" Hz:");
   lcd.print(flowrate);
-  //lcd.print(flowrate);
+  #//lcd.print(flowrate);
   Serial.print("Freq: "); Serial.println(flowrate);
   Serial.print("Pulses: "); Serial.println(pulses, DEC);
 
-  // if a plastic sensor use the following calculation
+  #// if a plastic sensor use the following calculation
   // Sensor Frequency (Hz) = 7.5 * Q (Liters/min)
   // Liters = Q * time elapsed (seconds) / 60 (seconds/minute)
   // Liters = (Frequency (Pulses/second) / 7.5) * time elapsed (seconds) / 60
@@ -74,7 +76,7 @@ void loop()                     // run over and over again
   liters /= 60.0;
 
 /*
-  // if a brass sensor use the following calculation
+  #// if a brass sensor use the following calculation
   float liters = pulses;
   liters /= 8.1;
   liters -= 6;
