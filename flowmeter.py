0500e613435be7981dff4a9ce023cf07d48cb76b
Frantisek Rokusek <frantisek.rokusek@gmail.com>
2019-04-26 19:58:23 +0200

    first try of liter computation, has to be hardware tested

--- flowmeter.py
+++ flowmeter.py
@@ -23,31 +23,3 @@ while True:
         print('\ncaught keyboard interrupt!, bye')
         GPIO.cleanup()
         sys.exit()
-
-
-
-# une autre solution
-# import RPi.GPIO as GPIO
-# import time, sys
-
-# FLOW_SENSOR = 4
-
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-# global count
-# count = 0
-
-# def countPulse(channel):
-#    global count
-#    count = count+1
-#    print(count);
-# GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=countPulse)
-
-# while True:
-#     try:
-#         time.sleep(1)
-#     except KeyboardInterrupt:
-#         print('\ncaught keyboard interrupt!, bye')
-#         GPIO.cleanup()
-#         sys.exit()
