b62069c3a52c06d17f0e6c72181b027b7b4a4632
Frantisek Rokusek <frantisek.rokusek@gmail.com>
2019-04-26 19:30:23 +0200

    first try of a unique code

--- flowmeter.py
+++ flowmeter.py
@@ -1,33 +1,20 @@
 from gpiozero import LineSensor
 from signal import pause
-
-sensor = LineSensor(4)
-
-sensor.when_line = lambda: print('Line detected')
-sensor.when_no_line = lambda: print('No line detected')
-pause()
-
-
-
-
-
-# une autre solution
-import RPi.GPIO as GPIO
 import time, sys
 
-FLOW_SENSOR = 23
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+sensor = LineSensor(4)
 
 global count
 count = 0
 
-def countPulse(channel):
-   global count
-   count = count+1
-   print(count);
-GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=countPulse)
+def countPulse():
+    global count
+    count += 1
+    print(count);
+
+sensor.when_line = lambda: countPulse
+sensor.when_no_line = lambda: print('No line detected')
+pause()
 
 while True:
     try:
@@ -36,3 +23,31 @@ while True:
         print('\ncaught keyboard interrupt!, bye')
         GPIO.cleanup()
         sys.exit()
+
+
+
+# une autre solution
+# import RPi.GPIO as GPIO
+# import time, sys
+
+# FLOW_SENSOR = 4
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+# global count
+# count = 0
+
+# def countPulse(channel):
+#    global count
+#    count = count+1
+#    print(count);
+# GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=countPulse)
+
+# while True:
+#     try:
+#         time.sleep(1)
+#     except KeyboardInterrupt:
+#         print('\ncaught keyboard interrupt!, bye')
+#         GPIO.cleanup()
+#         sys.exit()
