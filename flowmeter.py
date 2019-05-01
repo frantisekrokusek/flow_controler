f33fa3998e6fc1162c986503316913f05870bbea
frantisekrokusek <frantisek.rokusek@gmail.com>
2019-05-01 08:55:50 +0200

    code is displaying corect data

--- flowmeter.py
+++ flowmeter.py
@@ -10,16 +10,7 @@ count = 0
 def countPulse():
     global count
     count += 1
-    print(count);
+    liters = count / (7.5 * 60)
+    print('%s Litres'%(liters));
 
-sensor.when_line = lambda: countPulse
-sensor.when_no_line = lambda: print('No line detected')
-pause()
-
-while True:
-    try:
-        time.sleep(1)
-    except KeyboardInterrupt:
-        print('\ncaught keyboard interrupt!, bye')
-        GPIO.cleanup()
-        sys.exit()
+sensor.when_line = countPulse
